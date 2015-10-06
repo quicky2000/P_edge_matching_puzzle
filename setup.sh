28815f0c587d2a0855cb646f20c9a1f388a70084
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-10-06 23:18:14 +0200

    add a check to ensure that environment variables required by EXT_microhttpd has been set

--- setup.sh
+++ setup.sh
@@ -4,4 +4,10 @@ export QUICKY_REPOSITORY
 cd ${QUICKY_REPOSITORY}/quicky_tools/setup
 . setup.sh
 cd ../../..
+if [ -z "$EXT_MICROHTTPD_INC_DIR" ]; then
+    echo "ERROR : you have to set EXT_MICROHTTPD_INC_DIR"
+fi  
+if [ -z "$EXT_MICROHTTPD_LIB_DIR" ]; then
+    echo "ERROR : you have to set EXT_MICROHTTPD_LIB_DIR"
+fi  
 #EOF
